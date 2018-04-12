5f87a1ec13aa0515f5f836d03362d4a07154e174
MsimeleloMambu <g16m0246@campus.ru.ac.za>
2018-04-12 15:19:03 +0200

    Unmolested version of the game

---
+++ Gui/Mill.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gui
+{
+    public class Mill
+    {
+        public bool isNew { get; set; }
+        public int Id {get; set;}
+
+        public int[] Positions { get; set; }
+
+        public Mill(int[] Positions, int Id = -1)
+        {
+            this.Positions = Positions;
+            isNew = false;
+            this.Id = Id;
+        }
+    }
+}
