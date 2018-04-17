4aeab80402fe9aea96f294ef846cf1c687f9296f
MsimeleloMambu <g16m0246@campus.ru.ac.za>
2018-04-17 16:07:43 +0200

    Cow class

--- Morabaraba/Program.cs
+++ Morabaraba/Program.cs
@@ -370,4 +370,32 @@ namespace Morabaraba
 
 
     }
+
+    public class Cow
+    {
+        public int Position { get; set; }
+        public char UserId { get; set; }
+        public int CowNumber { get; set; }
+        public int Id { get; set; }
+        public string ImageName { get; set; }
+
+
+        public Cow(int Position = -1, char UserId = ' ', int CowNumber = -1, int Id = -1)
+        {
+            this.Position = Position;
+            this.UserId = UserId;
+            this.CowNumber = CowNumber;
+            this.Id = Id;
+            getplayerImageSource();
+        }
+
+        private void getplayerImageSource()
+        {
+            if (Id == 0) ImageName = "/Gui;component/Images/redCow.png";
+            else if (Id == 1) ImageName = "/Gui;component/Images/blueCow.png";
+            else ImageName = "/Gui;component/Images/emptyCow.png";
+        }
+
+    }
+
 }
