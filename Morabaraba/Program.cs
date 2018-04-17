edd92a819eb8e13b0f2d38f1049ad458d6dc9aa2
MsimeleloMambu <g16m0246@campus.ru.ac.za>
2018-04-17 16:12:06 +0200

    Mill class

--- Morabaraba/Program.cs
+++ Morabaraba/Program.cs
@@ -749,4 +749,19 @@ namespace Morabaraba
         }
     }
 
+    public class Mill
+    {
+        public bool isNew { get; set; }
+        public int Id { get; set; }
+
+        public int[] Positions { get; set; }
+
+        public Mill(int[] Positions, int Id = -1)
+        {
+            this.Positions = Positions;
+            isNew = false;
+            this.Id = Id;
+        }
+    }
+
 }
