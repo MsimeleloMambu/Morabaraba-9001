d1c9ac2095e663b800c357667deb085ca42b1001
belleprincesse <g16l0585@ru.ac.za>
2018-04-18 04:09:38 +0200

    Tests for placement and update in code

--- Morabaraba.Test/Tests.cs
+++ Morabaraba.Test/Tests.cs
@@ -13,11 +13,11 @@ namespace Morabaraba.Test
         {
             // Arrange-Act-Assert
             IBoard checkboard = new Board();
-            string [] myboard = checkboard.getBoardlist();
+            string[] myboard = checkboard.getBoardlist();
             int check = 0;
             for (int i = 0; i < 24; i++)
             {
-                if(myboard[i] == " ")
+                if (myboard[i] == " ")
                 {
                     check++;
                 }
@@ -25,5 +25,122 @@ namespace Morabaraba.Test
             Assert.That(check == 24);
 
         }
+        [Test]
+        public void testBlackgetsFirstChance()
+        {
+            Game game = new Game();
+            string who = game.getCurrentPlayerID();
+            Assert.AreSame("black", who);
+        }
+        [Test]
+        public void CowsPlacedOnEmptySpaces()
+        {
+            Game game = new Game();
+            game.Placement("a1");
+
+
+
+            Assert.False(game.BlankSpace("a1"));
+
+        }
+        [Test]
+        public void Amaxof12Placementsperplayerareallowed()
+        {
+            //Sakhele
+
+            Game game = new Game();
+
+            game.Placement("d5");
+
+            game.Placement("d6");
+
+            game.Placement("d7");
+
+            game.Placement("e3");
+
+            game.Placement("e4");
+
+            game.Placement("e5");
+
+            game.Placement("f2");
+
+            game.Placement("f4");
+
+            game.Placement("f6");
+
+            game.Placement("g1");
+
+            game.Placement("g4");
+            game.Placement("g7");
+
+            game.Placement("d3");
+            game.Placement("a4"); //nomatter howmany you add  it will still be 12
+
+            Assert.AreEqual(game.getplaceNumBlack(), 12);
+
+        }
+        [Test]
+        public void NomovementduringPlacement()
+        {
+
+            Game game = new Game();
+
+            game.Placement("d5");
+
+            game.Placement("d6");
+
+            game.Placement("d7");
+
+            game.Placement("e3");
+
+            game.Placement("e4");
+
+            game.Placement("e5");
+
+            game.Placement("f2");
+
+            game.Placement("f4");
+
+            game.Placement("f6");
+
+            game.Placement("g1");
+
+            game.Placement("g4");
+
+
+
+            game.swapcurrentPlayer();
+
+            game.Placement("a1");
+
+            game.Placement("a4");
+
+            game.Placement("a7");
+
+            game.Placement("b2");
+
+            game.Placement("b4");
+
+            game.Placement("b6");
+
+            game.Placement("c3");
+
+            game.Placement("c4");
+
+            game.Placement("c5");
+
+            game.Placement("d1");
+
+            game.Placement("d2");
+
+            game.Moving("a1", "g7");
+            List<List<string>> a = game.player1CheckMoves();
+            
+
+            Assert.AreSame(a,null);//meaning its empty
+
+        }
+       
+        
     }
 }
--- Morabaraba/Interfaces.cs
+++ Morabaraba/Interfaces.cs
@@ -10,12 +10,19 @@ namespace Morabaraba
         //printboard
         //
         string [] getBoardlist();
+        void updateMoveBoard(string player, string Pos);
+        void updateBoardlist(string pos, string character);
+        void printBoard(string[] myboard);
     }
+  
     public interface IPlayer
     {
         //playerpositions
         //addmills
         //
+        string getCurrentState();
+        void AddPosition(string position);
+        void updatingstate();
     }
     public interface IReferee
     {
