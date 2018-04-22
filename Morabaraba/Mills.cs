2af213cbbbc20f124f356f4c6baf26b8481d04ea
PerwinkleStar <g12n1275@campus.ru.ac.za>
2018-04-22 11:21:11 +0200

    Change Mill List property in Mill class from List string to List of List string

--- Morabaraba/Mills.cs
+++ Morabaraba/Mills.cs
@@ -6,20 +6,12 @@ namespace Morabaraba
 {
     public class Mills
     {
-        public List<string> MillList { get; private set; }
+        public List<List<string>> MillList { get; private set; }
 
         public Mills()
         {
             this.MillList = MillList;
-        }
-       public void removePosition(string position) //killing the cow
-       {
-
-       }
-       public void AddPosition(string position) //Flying cow moves to a new position and then later calls for millchecker
-       {
-
-       }
+        }     
        
         //disband mill formation
         //check valid Mill formation
